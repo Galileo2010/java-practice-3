5afd79dd455d9e5e4df96aebcac96c298ffe1d1a
Galileo2010 <15549070600@163.com>
2019-03-22 19:03:08 +0800

    Fix Practice06-Pratice07

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,24 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        klass = 0;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach "+ (klass == 0 ? "No Class." : "Class " + klass + ".");
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,28 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        klass = null;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach "+ (klass == null ? "No Class." : klass.getDisplayName() + ".");
+    }
+
+    public String introduceWith(Student jerry) {
+        return super.introduce() + " I am a Teacher. I "+ (klass.getNumber() == jerry.getKlass().getNumber() ? "" : "don't ")+ "teach " + jerry.getName() + ".";
+    }
 }
