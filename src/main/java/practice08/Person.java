7c0dc7a038135d79559b1ba5e33cedd55db41377
Galileo2010 <15549070600@163.com>
2019-03-22 23:00:48 +0800

    Fix Pratice08

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,35 @@
 package practice08;
 
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String introduce() {
+        return "My name is "+name+"."+" I am "+age+" years old.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return id == ((Person)obj).id;
+    }
 }
