dba226d5b7771547e14065030ac77ea0d1f5554f
Galileo2010 <15549070600@163.com>
2019-03-22 18:27:36 +0800

    Fix Practice02-Practice05

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,24 @@
 package practice02;
 
 public class Person {
+    public String name;
+    public int age;
+
+    public Person() {
+    }
+
+    public Person(String tom, int i) {
+        name = tom;
+        age = i;
+    }
+
+    public String introduce(){
+        return "My name is "+name+"."+" I am "+age+" years old.";
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
 }
