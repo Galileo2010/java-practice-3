4d548830251d4a999fc1100c68fc9739db5402c6
Galileo2010 <15549070600@163.com>
2019-03-22 14:58:51 +0800

    Fix Practice01

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,21 @@
 package practice01;
 
 public class Person {
+    public String name;
+    public int age;
+
+    public Person(String tom, int i) {
+        name = tom;
+        age = i;
+    }
+
+    public String introduce(){
+        return "My name is "+name+"."+" I am "+age+" years old.";
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
 }
