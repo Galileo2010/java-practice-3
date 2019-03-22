e4bae2e8d9a9ed3aa9f8b0788dac6df92c3b4a52
Galileo2010 <15549070600@163.com>
2019-03-22 23:26:50 +0800

    Fix Pratice09

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,29 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        klass = null;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach "+ (klass == null ? "No Class." : klass.getDisplayName() + ".");
+    }
+
+    public String introduceWith(Student jerry) {
+        return super.introduce() + " I am a Teacher. I "+ (klass.getNumber() == jerry.getKlass().getNumber() ? "" : "don't ")+ "teach " + jerry.getName() + ".";
+
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
